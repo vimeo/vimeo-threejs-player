b26722fc4b3b3cb2b85acfec8940e03e6af57054
juniorxsound <fleisher.or@gmail.com>
2019-02-12 13:40:14 -0500

    Adding callback for writeFile in Glitch readme fix script

--- assets/js/readme-glitch-fix.js
+++ assets/js/readme-glitch-fix.js
@@ -1,9 +1,11 @@
 const fs = require('fs');
 require('dotenv').config();
 
-var glitchReadMeText = "#Hello and welcome to the Vimeo three.js player examples 👋🏼 \n Have a look at our [Getting Started guide](https://github.com/vimeo/vimeo-threejs-player/wiki/Getting-Started-%F0%9F%9A%80), or check out the [full documentation in Github](https://github.com/vimeo/vimeo-threejs-player). \n For questions and support, ask on [StackOverflow](https://stackoverflow.com/questions/ask/?tags=vimeo)";
+var glitchReadMeText = "### Hello and welcome to the Vimeo three.js player examples 👋🏼 \n Have a look at our [Getting Started guide](https://github.com/vimeo/vimeo-threejs-player/wiki/Getting-Started-%F0%9F%9A%80), or check out the [full documentation in Github](https://github.com/vimeo/vimeo-threejs-player). \n For questions and support, ask on [StackOverflow](https://stackoverflow.com/questions/ask/?tags=vimeo)";
 
 if (process.env.ENV === 'Glitch') {
   console.log('[Ops] Changing the README in Glitch to point to Github documentation');
-  fs.writeFile('./README.md', glitchReadMeText);
+  fs.writeFile('./README.md', glitchReadMeText, function(err) {
+    console.log(err);
+  });
 }
